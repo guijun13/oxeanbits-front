36b8dd8224864aaa9e6988882e929faf20a6a4cc
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-05 15:26:55 -0300

    feat: add your score column to movies table

--- src/routes/movies.tsx
+++ src/routes/movies.tsx
@@ -1,13 +1,40 @@
 import '@progress/kendo-theme-default/dist/all.css';
-import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import {
+  Grid,
+  GridColumn as Column,
+  GridItemChangeEvent,
+  GridRowClickEvent,
+} from '@progress/kendo-react-grid';
 import { Button } from '@progress/kendo-react-buttons';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import TopLogin from '../components/topLogin';
 import { useAuth } from '../hooks/useAuth';
 
+interface Movies {
+  id: number;
+  title: string;
+  director: string;
+  average_score: number;
+  your_score?: number;
+}
+
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Array<Movies>>([]);
+  const [editID, setEditID] = useState<number | null>(null);
+
+  const rowClick = (event: GridRowClickEvent) => {
+    setEditID(event.dataItem.id);
+  };
+
+  const itemChange = (event: GridItemChangeEvent) => {
+    const inEditID = event.dataItem.id;
+    const field = event.field || '';
+    const newData = movies.map((item) =>
+      item.id === inEditID ? { ...item, [field]: event.value } : item
+    );
+    setMovies(newData);
+  };
 
   const { logout } = useAuth();
 
@@ -40,12 +67,19 @@ export default function Movies() {
           style={{
             height: '400px',
           }}
-          data={movies}
+          data={movies.map((item) => ({
+            ...item,
+            inEdit: item.id === editID,
+          }))}
+          editField="inEdit"
+          onRowClick={rowClick}
+          onItemChange={itemChange}
         >
-          <Column field="id" title="ID" width="40px" />
-          <Column field="title" title="Title" width="250px" />
-          <Column field="director" title="Director" width="250px" />
-          <Column field="average_score" title="Rate" width="250px" />
+          <Column field="id" title="ID" width="40px" editable={false} />
+          <Column field="title" title="Title" width="250px" editable={false} />
+          <Column field="director" title="Director" width="250px" editable={false} />
+          <Column field="average_score" title="Average score" width="250px" editable={false} />
+          <Column field="your_score" title="Your score" width="250px" editor="numeric" />
         </Grid>
       </div>
     </div>
