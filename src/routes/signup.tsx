ae95c19be58ed2592d3f5741f43d4fee8bacc53b
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-05 14:47:51 -0300

    feat: integrate signup forms

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -3,12 +3,61 @@ import { Button } from '@progress/kendo-react-buttons';
 import TopLogin from '../components/topLogin';
 import FormInput from '../components/formInput';
 import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
 
 export default function Signup() {
-  const { logout } = useAuth();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { login, logout } = useAuth();
 
   const handleLogout = () => {
     logout();
+  };
+
+  const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          email,
+          password,
+          confirmPassword,
+        },
+      }),
+    }).then((res) => {
+      console.log(res);
+      if (res.status === 201) {
+        console.log('Signup successful');
+        login({ email, password });
+      } else {
+        console.error('Signup failed');
+      }
+    });
+    return true;
   };
 
   return (
@@ -16,15 +65,25 @@ export default function Signup() {
       <TopLogin handleLogout={handleLogout} />
       <div>
         <h1 className="text-xl font-bold">Register</h1>
-        <div className="row example-wrapper" style={{ minHeight: 450 }}>
+        <form onSubmit={handleSubmit} className="row example-wrapper" style={{ minHeight: 450 }}>
           <div className="col-12 col-md-6 example-col">
-            <FormInput type="text" labelText="Name" />
-            <FormInput type="email" labelText="Email" />
-            <FormInput type="password" labelText="Password" />
-            <FormInput type="password" labelText="Confirm password" />
+            <FormInput type="text" labelText="Name" value={username} onChange={handleUsername} />
+            <FormInput type="email" labelText="Email" value={email} onChange={handleEmail} />
+            <FormInput
+              type="password"
+              labelText="Password"
+              value={password}
+              onChange={handlePassword}
+            />
+            <FormInput
+              type="password"
+              labelText="Confirm password"
+              value={confirmPassword}
+              onChange={handleConfirmPassword}
+            />
           </div>
-          <Button>Register</Button>
-        </div>
+          <Button type="submit">Register</Button>
+        </form>
       </div>
     </div>
   );
